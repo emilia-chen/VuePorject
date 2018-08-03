583ebbec7d604012d157053d75e9530be8c73ac5
chenjiacheng <jiacheng.chen@hand-china.com>
2018-08-03 13:21:32 +0800

    vux

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,77 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  
+     <toolbar></toolbar>
+      <note-list></note-list>
+<editor></editor>
+  
+
+ <button @click="increment()">+</button>
+    <button @click="decrement()">-</button>
+    <p>{{count}}</p>
+   <p>"aaa:"+{{doneTodosCount}}</p>
   </div>
+
+
+
 </template>
 
 <script>
+import Toolbar from './components/Toolbar.vue'
+import NoteList from './components/NoteList.vue'
+import Editor from './components/Editor.vue'
+import {mapState,mapGetters, mapMutations, mapActions} from 'vuex';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Toolbar,
+    NoteList,
+    Editor
+  },
+
+  computed:{
+
+    
+     ...mapState(["count"]),
+     /*...mapState({
+            count:state=>state.count  
+     }),*/
+    /*count(){
+      return this.$store.state.count
+    },*/
+   /* doneTodosCount () {
+    return this.$store.getters.doneTodosCount
+  }*/
+   ...mapGetters([
+           "doneTodosCount"
+        ]),
+  },  
+   methods: {
+    /*increment () {
+      this.$store.commit('increment')
+    },
+    decrement () {
+      this.$store.commit('decrement')
+    }*/
+   /* ...mapMutations([  
+            'decrement','increment'
+        ]),*/
+
+    increment () {
+      this.$store.dispatch('increment')
+    },
+    decrement () {
+      this.$store.dispatch('decrement')
+    }     
+
+
+
+  }
 }
+//  <img src="./assets/logo.png">
+
+ //<router-view/>
 </script>
 
 <style>
@@ -19,5 +82,38 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+@import url(https://fonts.googleapis.com/css?family=Raleway:400,300);
+html, #app {
+  height: 100%;
+}
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  height: 100%;
+  max-height: 100%;
+  position: relative;
+}
+#toolbar {
+  float: left;
+  width: 80px;
+  height: 100%;
+  background-color: #30414D;
+  color: #767676;
+  padding: 35px 25px 25px 25px;
+}
+#notes-list {
+  float: left;
+  width: 300px;
+  height: 100%;
+  background-color: #F5F5F5;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 400;
+}
+#note-editor {
+  min-width: 600px;
+  height: 100%;
+  margin-left: 380px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,141 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
 
 Vue.config.productionTip = false
+
+import state from './state'
+
+
+// const moduleA = {
+//   state: { count: 1,},
+//   mutations: {
+//         increment(state) {
+//             state.count++
+//         },
+//         decrement(state) {
+//             state.count--
+//         }
+//     },
+//   actions: { 
+//       increment (context) {
+//       context.commit('increment')
+//     }
+//    },
+//   getters: {  }
+// }
+
+// const moduleB = {
+//   state: {   count1: 1,},
+//   mutations: { },
+//   actions: { 
+
+//   }
+// }
+
+// const store = new Vuex.Store({
+//   modules: {
+//     a: moduleA,
+//     b: moduleB
+//   }
+// })
+
+const store = new Vuex.Store({
+    state: {
+        count: 0,
+        todos: [{
+            id: 1,
+            text: '...',
+            done: true
+        }, {
+            id: 2,
+            text: '...',
+            done: false
+        }, {
+            id: 3,
+            text: '...',
+            done: true
+        }]
+    },
+    getters: {
+        doneTodos: state => {
+            return state.todos.filter(todo => todo.done)
+        },
+        doneTodosCount: (state, getters) => {
+            return getters.doneTodos.length;
+        }
+    },
+    mutations: {
+        increment(state) {
+           
+           state.count++;
+        },
+        decrement(state) {
+            state.count--
+        }
+    },
+    actions: {
+    increment (context) {
+    	console.log(context);
+    	context.commit('increment');
+    console.log('我比reduce提前执行');
+     // 
+    },
+    decrement (context) {
+        console.log(context);
+        context.commit('decrement');
+     }
+     // 
+    }
+  
+});
+//store.dispatch('increment');
+//store.commit('increment')
+
+//console.log(store.state.count) // -> 1
+console.log(store.a)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
